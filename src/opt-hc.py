10eead42f69f07b9e7612d0891db310434fb86f2
Yoshiki Satotani <yoshikimethod@hotmail.co.jp>
2018-05-03 13:11:23 +0900

    Add logging feature on hill-climbing optimization

--- src/opt-hc.py
+++ src/opt-hc.py
@@ -7,11 +7,17 @@ from argparse import ArgumentParser
 from graph_config import GraphConfig
 from edge_swap import random_swap
 
-def optimize(G, conf, steps):
+def optimize(G, conf, steps, log=None):
+    def write_log_row(t, diam, aspl, ofile):
+        print('{},{},{}'.format(t, diam, aspl), file=ofile)
+
     min_diam = nx.diameter(G)
     min_aspl = nx.average_shortest_path_length(G)
-    while steps > 0:
-        steps -= 1
+    if log:
+        log_fp = open(log, 'w')
+        print('t,diam,aspl', file=log_fp)
+        write_log_row(0, min_diam, min_aspl, log_fp)
+    for t in range(1, steps+1):
         H = G.copy()
         random_swap(H, conf)
         diam = nx.diameter(H)
@@ -19,6 +25,11 @@ def optimize(G, conf, steps):
         if diam < min_diam or\
            diam == min_diam and aspl < min_aspl:
             G, min_diam, min_aspl = H, diam, aspl
+        if log:
+            write_log_row(t, min_diam, min_aspl, log_fp)
+
+    if log:
+        log_fp.close()
     return G
 
 def get_options():
@@ -29,6 +40,7 @@ def get_options():
     parser.add_argument('-o', '--output', type=str)
     parser.add_argument('--seed', type=int)
     parser.add_argument('--step', type=int, default=1000)
+    parser.add_argument('--log', type=str)
     opts = parser.parse_args()
     return opts
 
@@ -36,7 +48,7 @@ def main():
     opts = get_options()
     random.seed(opts.seed)
     G = nx.read_edgelist(opts.input, nodetype=int)
-    G = optimize(G, GraphConfig(opts.size, opts.r), opts.step)
+    G = optimize(G, GraphConfig(opts.size, opts.r), opts.step, opts.log)
 
     os = open(opts.output, 'w') if opts.output else sys.stdout
     for e in nx.generate_edgelist(G, data=False):
